3cf0827287c47f766b9741088986ee07d7842cf8
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-08 21:09:24 -0400

    Adding snapshot testing

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -3,7 +3,8 @@ import React from "react";
 import Card from "./Card";
 
 it('expect to render Card component', () => {
-  expect(shallow(<Card/>).length).toEqual(1);
+  // expect(shallow(<Card/>).length).toEqual(1);
+  expect(shallow(<Card/>)).toMatchSnapshot();
 });
 
 
